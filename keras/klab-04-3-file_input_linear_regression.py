b47611208dcd7546a899193855757aa72da31a1f
BEN (XueBin) LI <lixuebinfq@gmail.com>
2018-03-14 15:16:17 -0500

    Update klab-04-3-file_input_linear_regression.py

--- keras/klab-04-3-file_input_linear_regression.py
+++ keras/klab-04-3-file_input_linear_regression.py
@@ -1,9 +1,11 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
+
 xy = np.loadtxt('data-01-test-score.csv', delimiter=',')
-x_data = xy[:, 0:-1]
-y_data = xy[:, [-1]]
+
+x_data = xy[:, 0:-1]   #从0列 至 倒数1列，x=[?,?,?]
+y_data = xy[:, [-1]]   #只是最后一列     y=?
 
 print("x_data", x_data)
 print("y_data", y_data)
