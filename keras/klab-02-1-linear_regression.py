49f0a9fb81c3f81953fd14e1988ba9ced0732248
BEN (XueBin) LI <lixuebinfq@gmail.com>
2018-03-14 16:41:18 -0500

    Update klab-02-1-linear_regression.py

--- keras/klab-02-1-linear_regression.py
+++ keras/klab-02-1-linear_regression.py
@@ -15,8 +15,15 @@ model.add(Dense(1, input_dim=1))
 model.summary()
 # prints summary of the model to the terminal
 
-sgd = optimizers.SGD(lr=0.1)
+sgd = optimizers.SGD(lr=0.1)  
+#keras.optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
+#随机梯度下降法，支持动量参数，支持学习衰减率，支持Nesterov动量
+#lr：大或等于0的浮点数，学习率
+#momentum：大或等于0的浮点数，动量参数
+#decay：大或等于0的浮点数，每次更新后的学习率衰减值
+#nesterov：布尔值，确定是否使用Nesterov动量
 model.compile(loss='mse', optimizer=sgd)
+#loss=mean_squared_error或mse
 model.fit(x_train, y_train, epochs=200)
 
 y_predict = model.predict(np.array([5]))
