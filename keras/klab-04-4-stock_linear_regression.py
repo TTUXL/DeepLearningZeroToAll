c523b12f75bca14afd6517724143690b87d2d478
BEN (XueBin) LI <lixuebinfq@gmail.com>
2018-03-14 16:00:38 -0500

    Update klab-04-4-stock_linear_regression.py

--- keras/klab-04-4-stock_linear_regression.py
+++ keras/klab-04-4-stock_linear_regression.py
@@ -1,3 +1,4 @@
+#这个程序是演示两种测试模型的方法，第一种是手动，第二种是分成两个数据集，自动测试。
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
@@ -8,6 +9,7 @@ import matplotlib.pyplot as plt
 xy = np.loadtxt('data-02-stock_daily.csv', delimiter=',')
 
 # very important. It does not work without it.
+# 归一化
 scaler = MinMaxScaler(feature_range=(0, 1))
 xy = scaler.fit_transform(xy)
 
@@ -31,11 +33,13 @@ model.add(Dense(input_dim=4, units=1))
 model.compile(loss='mse', optimizer='sgd', metrics=['mse'])
 model.fit(x_data, y_data, epochs=100)
 
+#手动测试一下训练效果，从data里取一个值，测试一下模型输出
 test = x_data[10].reshape(-1, 4)
 print("y=", y_data[10], "prediction=", model.predict(test))
-
+#y= 0.9003840704898083 prediction= [[0.9225873]]
 test = x_data[30].reshape(-1, 4)
 print("y=", y_data[30], "prediction=", model.predict(test))
+#y= 0.8786132057338312 prediction= [[0.89652085]]
 
 # ---------------------------
 # Test
@@ -57,6 +61,7 @@ model.fit(x_train, y_train, epochs=200)
 results = model.evaluate(x_test, y_test, verbose=1)
 print(results)
 
+#画出预测结果
 predictions = model.predict(x_test)
 
 plt.plot(y_test)
